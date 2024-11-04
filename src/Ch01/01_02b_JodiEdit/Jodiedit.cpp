2cfe006d2cdaaac4ab37753f2032d0dbbaa5e9bf
MrDubs-OS <jodi_jay@live.com>
2024-11-04 20:28:05 +0000

    eedit used with A modern approach

---
+++ src/Ch01/01_02b_JodiEdit/Jodiedit.cpp
@@ -0,0 +1,16 @@
+// Learning C++ 
+// Exercise 01_02
+// Hello World, by Eduardo Corpeño, test to change name or extention
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+printf("To C, or not to C: that is the question. \n"); 
+
+return 0;
+
+}
+
