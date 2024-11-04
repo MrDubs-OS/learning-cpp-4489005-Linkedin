c078e99bbb9e9ebf6825b3d516bd7f468069730b
MrDubs-OS <jodi_jay@live.com>
2024-11-04 19:41:15 +0000

    changing name

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Eduardo Corpeño, test to change name 
+
+#include <stdio.h>
+
+int main(void)
+
+{
+
+printf("To C, or not to C: that is the question. \n"); 
+
+return 0;
+
+}
+
